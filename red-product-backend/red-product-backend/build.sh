a2a459776b658e898dfbf3a30f31187252b31815
sabake.konare <sabakekonare55@gmail.com>
2026-07-19 20:00:14 +0000

    Création automatique du superuser

--- red-product-backend/red-product-backend/build.sh
+++ red-product-backend/red-product-backend/build.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
-# Script exécuté par Render à chaque déploiement.
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+name = os.getenv("DJANGO_SUPERUSER_NAME")
+
+if email and password and name:
+    if not User.objects.filter(email=email).exists():
+        User.objects.create_superuser(
+            email=email,
+            name=name,
+            password=password,
+        )
+        print("✅ Superutilisateur créé.")
+    else:
+        print("ℹ️ Le superutilisateur existe déjà.")
+else:
+    print("⚠️ Variables d'environnement manquantes.")
+EOF
